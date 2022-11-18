998da4f89aa26f5081de82bbb25b66d07f768344
chowais181 <f180269@nu.edu.pk>
2022-11-18 18:02:25 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# The hsntawanaNFT Token
+
+This is a demo app on how to deploy your own NFT to the Ethetereum Blockchain. The full article is available on the LogRocket blog - How to deploy your own NFT with JavaScript.
+
+![](https://ipfs.io/ipfs/QmYtPjW13RiaVvq56dr4FBD3txv4VQaKH6SxsnKFfkhz9g)
+
+## Getting started
+
+**To get started, follow the instructions below**
+
+- Clone this repo using `git clone https://github.com/onedebos/the-osun-river-nft.git`
+
+- Create a `.env` file. Then, follow the steps in the tutorial to obtain your
+  - METAMASK_PRIVATE_KEY
+  - [API_URL](https://alchemyapi.io) (From Alchemy)
+  - METAMASK_PUBLIC_KEY : this is basically the address of your metamask wallet
+
+## Run this example
+
+Once done with the Getting started bit, you can run this example using the steps below.
+
+- npm install (install all the required packages)
+- `npx hardhat compile` - to compile the smart contract.
+- `npx hardhat run scripts/deploy.js --network ropsten` - to run the deploy scripts and deploy the contract to the Ropsten Testnet.
+- node scripts/mint-nft.js
+
+## Built With
+
+- Metamask
+- Alchemy
+- IPFS
+- JavaScript
+- Solidity
+
+### Automated Tests
+
+- Currently, there are no automated tests avaailable for this project. This will be implemented at a later date.
+
+## Author
+
+👤 **Awais Zahid**
+
+- [github](https://github.com/chowais181)
+- [twitter](https://twitter.com/chaudhary_181)
+- [linkedin](https://www.linkedin.com/in/awais-zahid-790124197/)
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
---
+++ scripts/mint-nft.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const API_URL = process.env.API_URL;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const alchemyWeb3 = createAlchemyWeb3(API_URL);
+const contract = require("../artifacts/contracts/hsntawanaNft.sol/TorNFT.json");
+// creating an instance of our contract
+const contractAddress = "0x51234545850Fd3C1E7a0973804670Fb7e0583B0e";
+const nftContract = new alchemyWeb3.eth.Contract(contract.abi, contractAddress);
+
+const METAMASK_PUBLIC_KEY = process.env.METAMASK_PUBLIC_KEY;
+const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+
+async function mintNFT(tokenURI) {
+  // get the nonce - nonce is needed for security reasons. It keeps track of the number of
+  // transactions sent from our address and prevents replay attacks.
+  const nonce = await alchemyWeb3.eth.getTransactionCount(
+    METAMASK_PUBLIC_KEY,
+    "latest"
+  );
+  const tx = {
+    from: METAMASK_PUBLIC_KEY, // our MetaMask public key
+    to: contractAddress, // the smart contract address we want to interact with
+    nonce: nonce, // nonce with the no of transactions from our account
+    gas: 1000000, // fee estimate to complete the transaction
+    data: nftContract.methods
+      .createNFT("0x450846E7C4567e4c7881984d0f16d1b7E7081091", tokenURI)
+      .encodeABI(), // call the createNFT function from our hsntawanaNFT.sol file and pass the account that should receive the minted NFT.
+  };
+
+  const signPromise = alchemyWeb3.eth.accounts.signTransaction(
+    tx,
+    METAMASK_PRIVATE_KEY
+  );
+  signPromise
+    .then((signedTx) => {
+      alchemyWeb3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of our transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of our transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong when submitting our transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(" Promise failed:", err);
+    });
+}
+
+mintNFT("https://ipfs.io/ipfs/QmYtPjW13RiaVvq56dr4FBD3txv4VQaKH6SxsnKFfkhz9g") // pass the CID to the JSON file uploaded to Pinata
